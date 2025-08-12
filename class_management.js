8526d799333da655491834c924532ebf44f2b851
Jomarie <123220864+Jomarie0@users.noreply.github.com>
2025-08-13 00:06:40 +0800

    Update class_management.js

--- class_management.js
+++ class_management.js
@@ -10,37 +10,38 @@ let users = [
 ];
 
 app.get(`/users`, (req,res) => {
+    res.status(200).json(users);
+});
+
+app.post(`/users`, (req,res) => {
     const { firstName, lastName, section, status } = req.body;
     const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
 
     if (userIndex !== -1) {
-        user[userIndex].status = status;
-        console.log(`Updated attendance for ${lastName} ${firstName} to: ${status} `);
-        res.status(200).json({message: `Attendance for ${lastName} ${lastName} has been updated to ${status}. `});
-    } else {
-        const newUser = {
-            id: user.length +1,
-            lastName,
-            firstName,
-            section,
-            status
-        
-        };
-        users.push(newUser);
-        console.log(`New user Added ${lastName} ${firstName} to: ${status} `);
-        res.status(201).json({message: `New Student ${lastName} ${lastName} has been added with status: ${status}. `});
+        users[userIndex].status = status;
+        console.log(`Updated attendance for ${lastName} ${firstName} to: ${status}`);
+        return res.status(200).json({ message: `Attendance for ${lastName} ${firstName} has been updated to ${status}.` });
+    }
 
+    const newUser = {
+        id: users.length + 1,
+        firstName,
+        lastName,
+        section,
+        status
     };
-
-
+    users.push(newUser);
+    console.log(`New user added ${lastName} ${firstName} with status: ${status}`);
+    res.status(201).json({ message: `New student ${lastName} ${firstName} has been added with status: ${status}.` });
 });
 
-app.get(`users`, (req,res) => {
+
+app.get(`/users`, (req,res) => {
     res.status(200).json(users);
 });
 
 
-app.get(`/`, (res,req) => {
+app.get(`/`, (req,res) => {
     res.send(`server is up and running!`);
 });
 
